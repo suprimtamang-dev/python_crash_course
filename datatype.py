2afe2911242ebc458195e68fb3a1c2e8be59b0ee
Suprim Tamang <tamangsuprim35@gmail.com>
2026-06-04 08:06:18 +0545

    Add datatype.py

---
+++ datatype.py
@@ -0,0 +1,21 @@
+#datatypes
+
+#true or false -> boolean
+
+
+#string -> str
+
+
+name = "Suprim"
+
+#numbers -> int, float
+age = 20
+height = 5.3
+
+#printing the type of a variable
+x = 5
+print(type(x))
+
+print("Hello", name)
+print("You are", age, "years old.")
+print("Your height is", height, "feet.")
