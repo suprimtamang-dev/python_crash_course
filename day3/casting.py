b0b8cd142ddade24d4b80b6d2a7ab4432e5e6051
Suprim Tamang <87705379+suprimtamang-dev@users.noreply.github.com>
2026-06-04 18:13:30 +0545

    Force add casting.py

---
+++ day3/casting.py
@@ -0,0 +1,16 @@
+age =15
+print(type(age))
+
+age_float =int(12.5)
+print(age_float)
+print(type(age_float))
+
+short_note="This is a short note."
+print(short_note[::-1])
+name="Sandesh"
+age=19
+
+course="Python"
+print(f"Mya name is {name}, I am learning{course}. and i am {age} years old")
+print(f"The value of course is:{course}")
+print(f"the reverse of course is:{course[::-1]}")
