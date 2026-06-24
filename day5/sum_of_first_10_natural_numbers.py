4f621cd3592ab9dc789f100b62eae8e8d8a9f332
Suprim Tamang <87705379+suprimtamang-dev@users.noreply.github.com>
2026-06-24 22:01:44 +0545

    Save changes

--- day5/sum_of_first_10_natural_numbers.py
+++ day5/sum_of_first_10_natural_numbers.py
@@ -3,7 +3,7 @@ sum=0
 
 input_num=int(input("Enter the number of natural number: "))
 while num<=input_num:
-    sum=sum+num
-    num=num+1
+ sum=sum+num
+ num=num+1
 print("The sum of the natural numbers is:", sum)
 
---
+++ day6/readme.md
@@ -0,0 +1,3 @@
+### what i learned
+learned about append function which adds items to the list 
+also learned that else if is written as elif unlike c or java 
