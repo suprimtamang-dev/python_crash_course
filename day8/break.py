ac5db452dfae3843f901ad8dc12e20c0a34b7e83
Suprim Tamang <87705379+suprimtamang-dev@users.noreply.github.com>
2026-06-25 08:38:40 +0545

    day8 continue,break,and else practice

---
+++ day8/break.py
@@ -0,0 +1,10 @@
+    
+#total=sum(range(10))
+#print(total)
+
+
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(f"{n} equals {x} * {n//x}")
+            break
