90883c0d827fa53b149f750bc2f9aa7512109372
Suprim Tamang <87705379+suprimtamang-dev@users.noreply.github.com>
2026-06-25 06:45:04 +0545

    Added product.py to day7

---
+++ day7/product.py
@@ -0,0 +1,11 @@
+product={
+    "name":"bmw",
+    "stock": 20,
+    "status":"available"
+}
+active_product={
+    "name":product["name"],
+    "stock":product["stock"],
+    "status":product["status"]
+}
+print(active_product)
